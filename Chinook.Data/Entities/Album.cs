4e7505373d7b26db7b1fb26d780f123638d9f5b2
dglenn3 <35244607+dglenn3@users.noreply.github.com>
2018-01-17 18:43:47 -0700

    Added artist, album and track entities

---
+++ Chinook.Data/Entities/Album.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace Chinook.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+        [StringLength(160,ErrorMessage ="Title must not exceed 160 characters")]
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+        [StringLength(50,ErrorMessage ="Release label must not exceed 50 characters")]
+        public string ReleaseLabel { get; set; }
+
+        //navigational properties
+        public virtual Artist Artist { get; set; }
+        public virtual ICollection<Track> Tracks { get; set; }
+    }
+}
---
+++ Chinook.Data/Entities/Track.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace Chinook.Data.Entities
+{
+    [Table("Tracks")]
+    public class Track
+    {
+        [Key]
+        public int TrackId { get; set; }
+        [StringLength(200, ErrorMessage ="Name must not exceed 200 characters")]
+        public string Name { get; set; }
+        public int? AlbumId { get; set; }
+        public int MediaTypeId { get; set; }
+        public int? GenreId { get; set; }
+        [StringLength(220,ErrorMessage ="Composer must not exceed 220 characters")]
+        public string Composer { get; set; }
+        public int Milliseconds { get; set; }
+        public int? Bytes { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        //navigational properties
+        public virtual Album Album { get; set; }
+    }
+}
